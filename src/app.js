60b2cc13f4ec2b023fd719f5b49409f127a0d994
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-01 19:20:22 +0100

    add lenis & gsap

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
 import "./styles.css";
+import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+// Lenis smooth scroll
+const lenis = new Lenis();
+lenis.on("scroll", () => ScrollTrigger.update);
+
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+
+gsap.ticker.lagSmoothing(0);
